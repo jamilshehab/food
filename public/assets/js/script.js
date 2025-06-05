1e69644756249c4e75182cbb742ef9f409db02cd
jamilshehab <shehabjamil20@gmail.com>
2025-06-05 00:04:16 -0700

    Changes added

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,23 +1,6 @@
 AOS.init({
-<<<<<<< HEAD
-    duration: 1200,
-});
-const swiper = new Swiper(".swiper-container", {
-    loop: true,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    speed: 1000,
-});
-=======
   duration: 1200,
 })
 
    
   
->>>>>>> bfecfabc2714be9b2b6e16186235bb528f132114
