0c9e8a0570e9786ddcb69c5fb45dd16ae70eb32b
jamilshehab <shehabjamil20@gmail.com>
2025-06-04 23:28:08 -0700

    changes added and displayed

--- routes/web.php
+++ routes/web.php
@@ -14,30 +14,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/',  [HomeController::class,'index'])->name('home');
     
     Route::get('/dashboard', [DashboardController::class,'dashboard'])->name('dashboard');
-    //sliders
+   
+     
+    Route::resource('slider',SliderController::class)->except('show');
+    Route::resource('about',AboutController::class)->except('show');
+    Route::resource('menu',MenuController::class)->except('show');
     
-    Route::get('/viewslider',[SliderController::class,'index'])->name('slider.view');
-    Route::get('/addslider',[SliderController::class,'create'])->name('slider.create');
-    Route::post('/addslider',[SliderController::class,'store'])->name('slider.store');
-    Route::patch('/editslider',[SliderController::class,'update'])->name('slider.update');
-    Route::get('/sliders/{id}/edit', [SliderController::class, 'edit'])->name('slider.edit');
-    Route::put('/sliders/{id}', [SliderController::class, 'update'])->name('slider.update');
-
-    Route::delete('/sliders/{id}', [SliderController::class, 'destroy'])->name('slider.destroy'); 
-    Route::get('/viewabout',[AboutController::class,'index'])->name('about.view');
-    Route::get('/addabout', action:[AboutController::class,'create'])->name('about.create');
-    Route::get('/about/{id}/edit', [AboutController::class, 'edit'])->name('about.edit');
-    Route::put('/about/{id}', [AboutController::class, 'update'])->name('about.update');
-    Route::post('/addabout',[AboutController::class,'store'])->name('about.store');
-    Route::delete('/about/{id}', [AboutController::class, 'destroy'])->name('about.destroy'); 
-
-    Route::get('/viewmenu',[MenuController::class,'index'])->name('menu.view');
-    Route::get('/addmenu', action:[MenuController::class,'create'])->name('menu.create');
-    Route::post('/addmenu',[MenuController::class,'store'])->name('menu.store');
-
-    Route::get('/menu/{id}/edit', [MenuController::class, 'edit'])->name('menu.edit');
-    Route::put('/menu/{id}', [MenuController::class, 'update'])->name('menu.update');
-    Route::delete('/menu/{id}', [MenuController::class, 'destroy'])->name('menu.destroy'); 
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
